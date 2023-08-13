f2c8f6b47530be5ba0a4c330138006487f243170
Typer002 <142151137+typer002@users.noreply.github.com>
2023-08-13 19:05:04 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,164 +1,62 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'component/my_textfield.dart';
-import 'component/my_button.dart';
-import 'screens/homepage.dart';
-import 'screens/register.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
-}
+void main() => runApp(const Index());
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  //Text Editing controllers
-  final usernameController = TextEditingController();
-  final passwordController = TextEditingController();
-  final formkey = GlobalKey<FormState>();
+class Index extends StatelessWidget {
+  const Index({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Hello",
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          body: Form(
-            key: formkey,
-            child: Center(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Icon(
-                    Icons.account_box,
-                    size: 100,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Welcome To Login Page",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  MyTextField(
-                    controller: usernameController,
-                    labelText: "Username",
-                    obcureText: false,
-                    check: (name) {
-                      if (name!.isEmpty) {
-                        return "กรุณาใส่ชื่อ";
-                      } else {
-                        return null;
-                      }
-                    },
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  MyTextField(
-                    controller: passwordController,
-                    labelText: "Password",
-                    obcureText: true,
-                    check: (password) {
-                      if (password!.isEmpty) {
-                        return "กรุณากรอกรหัสผ่าน";
-                      } else {
-                        return null;
-                      }
-                    },
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text("RememberMe"),
-                        Expanded(
-                          child: Text(
-                            "Forgot Password?",
-                            textAlign: TextAlign.right,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ), // Padding Forgot Password
-                  const SizedBox(
-                    height: 20,
-                  ),
+      home: const Calculatetor(),
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+    );
+  }
+}
 
-                  SizedBox(
-                    width: 390,
-                    height: 45,
-                    child: MyBotton(
-                        onpressed: () {
-                          if (formkey.currentState!.validate())
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => HomePage()));
-                        },
-                        color: Colors.blueAccent,
-                        btntext: "เข้าสู่ระบบ"),
-                  ),
+class Calculatetor extends StatefulWidget {
+  const Calculatetor({super.key});
+  @override
+  State<Calculatetor> createState() => _Calculatetor();
+}
 
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Register()));
-                    },
-                    child: const Text(
-                      "ยังไม่มีบัญชีใช้ ?",
-                      style: TextStyle(decoration: TextDecoration.underline),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
+class _Calculatetor extends State<Calculatetor> {
+  String titleAppbar = "Calculator";
+  TextEditingController inputNumber1 = TextEditingController();
+  TextEditingController inputNumber2 = TextEditingController();
+  int sum = 0;
+  int _calculate() {
+    int? num1 = int?.parse(inputNumber1.text);
+    int? num2 = int?.parse(inputNumber2.text);
+    sum = num1 + num2;
+    return sum;
+  }
 
-                  SizedBox(
-                    width: 390,
-                    height: 45,
-                    child: MyBotton(
-                        onpressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomePage()));
-                        },
-                        color: Colors.redAccent,
-                        btntext: "Login With Google"),
-                  ),
-
-                  SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    width: 390,
-                    height: 45,
-                    child: MyBotton(
-                        onpressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => HomePage()));
-                        },
-                        color: Colors.black,
-                        btntext: "Login With AppleID"),
-                  ),
-                ],
-              ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(titleAppbar),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TextField(
+              controller: inputNumber1,
+              decoration: const InputDecoration(labelText: "Enter number 1"),
             ),
-          ),
+            TextField(
+              controller: inputNumber2,
+              decoration: const InputDecoration(labelText: "Input number 2"),
+            ),
+            FloatingActionButton.small(
+              onPressed: _calculate,
+              child: Text("Calcaulate"),
+            ),
+            Text(sum.toString())
+          ],
         ));
   }
 }
